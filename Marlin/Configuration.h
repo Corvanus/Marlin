c8220c9440c16c5aa957f431a7627ed16000386b
Corvanus <corvanus@hotmail.com>
2020-10-15 18:12:01 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -25,7 +25,6 @@
 // #define V6_330_TITAN_TMC 1
 // #define V6_330_TITAN_NO_TMC 1
 #define V6_330_TITAN_NO_TMC 1
-// #define V6_330_NO_TITAN_TMC 1
 #define V6_330_NO_TITAN_TMC 1
 // #define V6_330_NO_TITAN_NO_TMC 1
 // #define V6_400_TITAN_TMC 1
